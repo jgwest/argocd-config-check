5d0bb1adafc9bd0c7de35ddb3b63cac6fff9cb8a
Jonathan West <jgwest@gmail.com>
2025-12-08 07:27:59 -0500

    Add check for unsupported custom images and further deprecated field checks
    Signed-off-by: Jonathan West <jgwest@gmail.com>

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 
 	var abstractK8sClient clients.AbstractK8sClient
 
+	// TODO: Attempt to determine the operator version, and output it. Then output reference to openshift gitops lifecycle list.
+
 	if len(os.Args) == 1 {
 		var err error
 		abstractK8sClient, err = clients.SystemK8sClient()
@@ -28,6 +30,7 @@ func main() {
 		if err != nil {
 			failWithError("unable to retrieve OMC client data from '"+pathToOMCDirectory+"'", err)
 		}
+
 	} else {
 		outputStatusMessage("Unexpected number of arguments. Valid parameters are:")
 		outputStatusMessage("")
@@ -89,17 +92,127 @@ func checkIndividualArgoCDCR(argoCD v1beta1.ArgoCD) *[]issue {
 	issues := []issue{}
 
 	checkArgoCDCRForDeprecatedFields(argoCD, &issues)
+	checkArgoCDCRForUnsupportedCustomImages(argoCD, &issues)
 
 	return &issues
 
 }
 
+// checkArgoCDCRForUnsupportedCustomImages identifies the use of custom container images for components where that is not supported.
+func checkArgoCDCRForUnsupportedCustomImages(argoCD v1beta1.ArgoCD, issues *[]issue) {
+
+	if argoCD.Spec.ApplicationSet != nil && len(argoCD.Spec.ApplicationSet.Image) > 0 {
+
+		*issues = append(*issues, issue{
+			field:   ".spec.applicationSet.image",
+			message: "The image field is used to provide custom container images for Argo CD components. However, specifying custom images for essential Argo CD components is not supported.",
+		})
+
+	}
+
+	if argoCD.Spec.SSO != nil && argoCD.Spec.SSO.Dex != nil && len(argoCD.Spec.SSO.Dex.Image) > 0 {
+
+		*issues = append(*issues, issue{
+			field:   ".spec.sso.dex.image",
+			message: "The image field is used to provide custom container images for Argo CD components. However, specifying custom images for essential Argo CD components is not supported.",
+		})
+
+	}
+
+	if len(argoCD.Spec.HA.RedisProxyImage) > 0 {
+
+		*issues = append(*issues, issue{
+			field:   ".spec.ha.redisProxyImage",
+			message: "The image field is used to provide custom container images for Argo CD components. However, specifying custom images for essential Argo CD components is not supported.",
+		})
+
+	}
+
+	if argoCD.Spec.ArgoCDAgent != nil {
+
+		if argoCD.Spec.ArgoCDAgent.Agent != nil && len(argoCD.Spec.ArgoCDAgent.Agent.Image) > 0 {
+			*issues = append(*issues, issue{
+				field:   ".spec.argoCDAgent.agent.image",
+				message: "The image field is used to provide custom container images for Argo CD components. However, specifying custom images for essential Argo CD components is not supported.",
+			})
+		}
+
+		if argoCD.Spec.ArgoCDAgent.Principal != nil && len(argoCD.Spec.ArgoCDAgent.Principal.Image) > 0 {
+
+			*issues = append(*issues, issue{
+				field:   ".spec.argoCDAgent.principal.image",
+				message: "The image field is used to provide custom container images for Argo CD components. However, specifying custom images for essential Argo CD components is not supported.",
+			})
+
+		}
+
+	}
+
+	if len(argoCD.Spec.Notifications.Image) > 0 {
+		*issues = append(*issues, issue{
+			field:   ".spec.notifications.image",
+			message: "The image field is used to provide custom container images for Argo CD components. However, specifying custom images for essential Argo CD components is not supported.",
+		})
+	}
+
+	if len(argoCD.Spec.Redis.Image) > 0 {
+		*issues = append(*issues, issue{
+			field:   ".spec.redis.image",
+			message: "The image field is used to provide custom container images for Argo CD components. However, specifying custom images for essential Argo CD components is not supported.",
+		})
+	}
+
+	if len(argoCD.Spec.Repo.Image) > 0 {
+		*issues = append(*issues, issue{
+			field:   ".spec.repo.image",
+			message: "The image field is used to provide custom container images for Argo CD components. However, specifying custom images for essential Argo CD components is not supported.",
+		})
+	}
+
+	if len(argoCD.Spec.Image) > 0 {
+		*issues = append(*issues, issue{
+			field:   ".spec.image",
+			message: "The image field is used to provide custom container images for Argo CD components. However, specifying custom images for essential Argo CD components is not supported.",
+		})
+	}
+
+}
+
+// checkArgoCDCRForDeprecatedFields identifies fields that are deprecated and no longer supported by ArgoCD operator.
 func checkArgoCDCRForDeprecatedFields(argoCD v1beta1.ArgoCD, issues *[]issue) {
 
 	if len(argoCD.Spec.ConfigManagementPlugins) > 0 {
 		*issues = append(*issues, issue{
 			field:   ".spec.configMapPlugins",
 			message: "ConfigManagementPlugins field is no longer supported. Argo CD now requires plugins to be defined as sidecar containers of repo server component. See '.spec.repo.sidecarContainers'. ConfigManagementPlugins was previously used to specify additional config management plugins.",
+		})
+	}
+
+	if argoCD.Spec.Grafana.Enabled {
+		*issues = append(*issues, issue{
+			field:   ".spec.grafana",
+			message: "grafana field is deprecated from ArgoCD CR: this field will be ignored by operator, and any remaining Grafana resources will be removed.",
+		})
+	}
+
+	if len(argoCD.Spec.InitialRepositories) > 0 {
+		*issues = append(*issues, issue{
+			field:   ".spec.initialRepositories",
+			message: "initialRepositories field is deprecated from ArgoCD CR. The field will be ignored by operator.",
+		})
+	}
+
+	if len(argoCD.Spec.RepositoryCredentials) > 0 {
+		*issues = append(*issues, issue{
+			field:   ".spec.repositoryCredentials",
+			message: "repositoryCredentials field is deprecated from ArgoCD CR. The field will be ignored by operator.",
+		})
+	}
+
+	if argoCD.Spec.SSO != nil && argoCD.Spec.SSO.Keycloak != nil {
+		*issues = append(*issues, issue{
+			field:   ".spec.sso.keycloak",
+			message: "keycloak field is no longer supported. ArgoCD operator will no longer create and manage a keycloak instance on the users behalf. Users may instead manage their own keycloak instance (using e.g. keycloak operator) and configure Argo CD to use it.",
 		})
 	}
 
